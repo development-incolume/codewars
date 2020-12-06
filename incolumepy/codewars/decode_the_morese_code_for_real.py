599827a3d025e9c293e73f400fa93a7cb34556b4
britodfbr <britodfbr@gmail.com>
2020-12-06 17:19:23 -0300

    estrutura inicial decode_the_morese_code_for_real

--- incolumepy/codewars/decode_the_morese_code_for_real.py
+++ incolumepy/codewars/decode_the_morese_code_for_real.py
@@ -1,3 +1,29 @@
 # /bin/env python
 # -*- encode: utf-8 -*-
 __author__ = '@britodfbr'
+
+__url__ = "https://www.codewars.com/kata/54acd76f7207c6a2880012bb/train/python"
+
+
+def decodeMorse(morseCode):
+    result = None
+
+    print(result)
+    return result
+
+
+def decodeBitsAdvanced(bits):
+    result = None
+    
+    print(result)
+    return result
+
+
+def run():
+    decodeBitsAdvanced('000000001101101001110000011000000111111010011111001111110000000000011101111111101111101'
+                       '1111000000101100011111100000111110011101100000100000')
+    decodeMorse('···· · −·−−   ·−−− ··− −·· ·')
+
+
+if __name__ == '__main__':
+    run()
