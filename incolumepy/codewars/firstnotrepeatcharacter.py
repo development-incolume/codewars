4681b0ec6c8306198550424d538b790bc7369113
britodfbr <britodfbr@gmail.com>
2020-12-06 08:15:22 -0300

    Atualizado para atender as especificações dos testes codewars

--- incolumepy/codewars/firstnotrepeatcharacter.py
+++ incolumepy/codewars/firstnotrepeatcharacter.py
@@ -8,14 +8,17 @@ from collections import Counter, OrderedDict
 def first_non_repeating_letter(string):
     d = OrderedDict()
     for i in string:
-        if i not in d:
+        if i.lower() not in [x.lower() for x in d]:
             d[i] = 1
         else:
-            d[i] += 1
+            try:
+                d[i] += 1
+            except KeyError:
+                d[i.swapcase()] += 1
 
     result = [a[0] for a in d.items() if a[1] < 2]
-    # print(string)
-    # print(d, result)
+    print(string)
+    print(d, result)
     if not result:
         return ''
     return result[0]
