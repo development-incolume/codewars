9f3d70619e46f2fb73d487b95da18a881f57e7e8
Britodfbr <britodfbr@gmail.com>
2020-12-27 07:32:02 -0300

    correção na separação entre fib/productFib

--- incolumepy/codewars/Product_of_consecutive_Fib_numbers.py
+++ incolumepy/codewars/Product_of_consecutive_Fib_numbers.py
@@ -8,11 +8,10 @@ import time
 @lru_cache(maxsize=64)
 def fib(n):
     # 1, 1, 2, 3, 5, ..
-    @lru_cache(maxsize=64)
-    def fib(n):
-        if n < 2:
-            return n
-        return fib(n-1) + fib(n-2)
+    if n < 2:
+        return n
+    return fib(n-1) + fib(n-2)
+
 
 def productFib(prod):
     mult = 0
