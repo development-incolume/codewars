12f72b87fd3d3a281596583a1cf1ea19203d5b99
Britodfbr <britodfbr@gmail.com>
2020-12-27 07:27:52 -0300

    Atualização dos testes para fib

--- tests/test_Product_of_consegutive_Fib_numbers.py
+++ tests/test_Product_of_consegutive_Fib_numbers.py
@@ -7,6 +7,7 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(fib(0), 1)
         self.assertEqual(fib(1), 1)
         self.assertEqual(fib(2), 1)
+        self.assertEqual(fib(-1), 1)
 
     def test_basics(self):
         self.assertEqual(productFib(4895), [55, 89, True])
