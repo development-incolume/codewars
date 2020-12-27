498a4651ef08544ba41ac978d3fa04531e00c6a1
Britodfbr <britodfbr@gmail.com>
2020-12-27 10:10:23 -0300

    Tratativa para atender testes avançados, dom 27 dez 2020 10:10:23 -03

--- incolumepy/codewars/String_incrementer.py
+++ incolumepy/codewars/String_incrementer.py
@@ -6,15 +6,16 @@ __author__ = '@britodfbr'
 
 
 def increment_string(s: str):
-    regex = r'(.+)(\d+)'
+    regex = r'(\w+)(\d+)'
     matches = re.compile(regex).search(s)
     if matches:
         print(matches.groups())
-    # base = matches.group(0)
-    # number = matches.group(1) or 0
-    # digits = len(number)
-    # print(matches.groups(), base, number, digits)
-    # strout = '{}{:0>%d}' % digits
-    # num = int(number) + 1
-    # result = strout.format(base, num)
+    base = matches.group(0) if matches else ""
+    number = matches.group(1) if matches else "0"
+    digits = len(number)
+    print(base, number, digits)
+    strout = '{}{:0>%d}' % digits
+    num = int(number) + 1
+    result = strout.format(base, num)
+    print(result)
     # return result
