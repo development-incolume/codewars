55ae1d6d2682b1fdda2a456fdc79797c47eb47b3
Britodfbr <britodfbr@gmail.com>
2020-12-27 07:16:18 -0300

    Teste individual para fib

--- tests/test_Product_of_consegutive_Fib_numbers.py
+++ tests/test_Product_of_consegutive_Fib_numbers.py
@@ -1,8 +1,13 @@
 import unittest
-from incolumepy.codewars.Product_of_consecutive_Fib_numbers import productFib
+from incolumepy.codewars.Product_of_consecutive_Fib_numbers import productFib, fib
 
 
 class MyTestCase(unittest.TestCase):
+    def test_fib(self):
+        self.assertEqual(fib(0), 1)
+        self.assertEqual(fib(1), 1)
+        self.assertEqual(fib(2), 1)
+
     def test_basics(self):
         self.assertEqual(productFib(4895), [55, 89, True])
         self.assertEqual(productFib(5895), [89, 144, False])
