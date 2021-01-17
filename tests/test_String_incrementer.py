5a076527c8f062b1a8c6aca91f2c4a1addfcb84b
Britodfbr <britodfbr@gmail.com>
2021-01-17 09:29:46 -0300

    Fatoração de testes

--- tests/test_String_incrementer.py
+++ tests/test_String_incrementer.py
@@ -10,14 +10,15 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(increment_string("foobar00"), "foobar01")
         self.assertEqual(increment_string("foobar99"), "foobar100")
         self.assertEqual(increment_string("foobar099"), "foobar100")
-        self.assertEqual(increment_string(""), "1")
 
     def test_avanceds(self):
-        self.assertEqual(increment_string("q.~2871332009"), "q.~2871332010")
+        self.assertEqual(increment_string(""), "1")
         self.assertEqual(increment_string(None), '1')
         self.assertEqual(increment_string('1'), '2')
-        self.assertEqual(increment_string('J.Z9l07816203>^jEG{|jb839460@s_ Cm"9550009'),
-                         'J.Z9l07816203>^jEG{|jb839460@s_ Cm"9550010')
+        self.assertEqual(increment_string("q.~2871332009"), "q.~2871332010")
+        self.assertEqual(
+            increment_string('J.Z9l07816203>^jEG{|jb839460@s_ Cm"9550009'),
+            'J.Z9l07816203>^jEG{|jb839460@s_ Cm"9550010')
 
 
 if __name__ == '__main__':
