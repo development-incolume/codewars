d5cac626564fcab39c9b88c51869440170494f42
Britodfbr <britodfbr@gmail.com>
2021-01-17 10:34:25 -0300

    Novo teste unitário para string incrementer

--- tests/test_String_incrementer.py
+++ tests/test_String_incrementer.py
@@ -11,22 +11,28 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(increment_string("foobar99"), "foobar100")
         self.assertEqual(increment_string("foobar099"), "foobar100")
 
-    def test_avanced0(self):
+    def test_advanced0(self):
         self.assertEqual(increment_string(""), "1")
 
-    def test_avanced1(self):
+    def test_advanced1(self):
         self.assertEqual(increment_string(None), '1')
 
-    def test_avanced2(self):
+    def test_advanced2(self):
         self.assertEqual(increment_string('1'), '2')
 
-    def test_avanced3(self):
+    def test_advanced3(self):
         self.assertEqual(increment_string("q.~2871332009"), "q.~2871332010")
 
-    def test_avanced4(self):
+    def test_advanced4(self):
         self.assertEqual(
             increment_string('J.Z9l07816203>^jEG{|jb839460@s_ Cm"9550009'),
             'J.Z9l07816203>^jEG{|jb839460@s_ Cm"9550010')
+
+    def test_advanced5(self):
+        self.assertEqual(
+            increment_string("qE7yOPr!71726313\"0/:.(H1.#R3066`^'Nz[r>t4186675470008615188699"),
+            'qE7yOPr!71726313"0/:.(H1.#R3066`^\'Nz[r>t4186675470008615188700'
+        )
 
 
 if __name__ == '__main__':
