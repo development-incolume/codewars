ed586631b39653396680c5cb2009f1395652067c
Britodfbr <britodfbr@gmail.com>
2021-01-17 08:44:16 -0300

    Correção em captura de fragmentos no regex

--- incolumepy/codewars/String_incrementer.py
+++ incolumepy/codewars/String_incrementer.py
@@ -1,21 +1,25 @@
 # /bin/env python
 # -*- encode: utf-8 -*-
-import re
-
 __author__ = '@britodfbr'
+import re
 
 
 def increment_string(s: str):
     regex = r'([a-z\W]+)(\d+)'
     matches = re.compile(regex).search(s)
-    if matches:
-        print(matches.groups())
-    base = matches.group(0) if matches else ""
-    number = matches.group(1) if matches else "0"
+
+    print(f'{s}:{matches.groups()}, {matches.group(0)}, {matches.group(1)}, {matches.group(2)}')
+    base = matches.group(1)
+    number = matches.group(2)
     digits = len(number)
-    print(base, number, digits)
+    print(f'{s}: {base}, {matches.groups()}, {number}, {digits}')
     strout = '{}{:0>%d}' % digits
     num = int(number) + 1
     result = strout.format(base, num)
     print(result)
     return result
+
+
+if __name__ == '__main__':
+    print(increment_string('asdfg01'))
+    print(increment_string('g.~1234567890'))
