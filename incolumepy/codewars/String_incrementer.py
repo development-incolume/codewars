11ee5b743651feaf4ef9c6c2f51e713f215b383b
Britodfbr <britodfbr@gmail.com>
2021-01-17 09:56:47 -0300

    Tratativa de correção, dom 17 jan 2021 09:56:47 -03

--- incolumepy/codewars/String_incrementer.py
+++ incolumepy/codewars/String_incrementer.py
@@ -9,11 +9,22 @@ def increment_string(s: str):
         return "1"
     regex = r'([a-z\W]+)(\d+)?'
     matches = re.compile(regex).search(s)
-    print(f'1.. {s}:{matches.groups()}, {matches.group(0)}, {matches.group(1)}, {matches.group(2)}')
-    base = matches.group(1) or ""
-    number = matches.group(2) or '0'
+    try:
+        print(f'1.. {s}:{matches.groups()}, {matches.group(0)}, {matches.group(1)}, {matches.group(2)}')
+    except AttributeError:
+        print('No Match regex')
+
+    try:
+        base = matches.group(1)
+    except AttributeError:
+        base = ""
+
+    try:
+        number = matches.group(2)
+    except TypeError:
+        number = s if s.isdigit() else '0'
     digits = len(number)
-    print(f'2.. {s}: {base}, {matches.groups()}, {number}, {digits}')
+    # print(f'2.. {s}: {base}, {matches.groups()}, {number}, {digits}')
     strout = '{}{:0>%d}' % digits
     num = int(number) + 1
     result = strout.format(base, num)
