e276125adfcede2aff2bd7e630eca361ad593bf4
Britodfbr <britodfbr@gmail.com>
2021-02-06 12:12:52 -0300

    [Last_digit_of_a_huge_number] Tratativas em sáb 06 fev 2021 12:12:52 -03

--- incolumepy/codewars/Last_digit_of_a_huge_number.py
+++ incolumepy/codewars/Last_digit_of_a_huge_number.py
@@ -1,20 +1,31 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 __author__ = '@britodfbr'
+import math
 
 
 def pow_list(lst: list) -> int:
     if not lst:
         return 1
-    return lst.pop(0) ** last_digit(lst)
+    return lst.pop(0) ** pow_list(lst)
+
+
+def pow_lst(lst: list) -> int:
+    if not lst:
+        return 1
+    return pow(lst.pop(0), pow_lst(lst))
 
 
 def last_digit(lst: list) -> int:
-    n = str(pow_list(lst))
+    n = str(pow_lst(lst))
+    print(n)
     return int(n[-1])
 
 
 if __name__ == '__main__':
-    print(last_digit([2, 2, 2, 2]))
+    # print(pow_list([2, 2]))
+    # print(pow_lst([2, 2]))
     print(last_digit([0, 0]))
-    print(pow_list([7, 6, 21]))
+    print(last_digit([2, 2, 2, 2]))
+    print(last_digit([6, 21]))
+    print(last_digit([7, 6, 21]))
