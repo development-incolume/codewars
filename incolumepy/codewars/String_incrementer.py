6304e2510b3a825e4822440ac3abae7c7ebaa8f8
Britodfbr <britodfbr@gmail.com>
2021-02-06 10:26:11 -0300

    [String_incrementer] finalizado com sucesso

--- incolumepy/codewars/String_incrementer.py
+++ incolumepy/codewars/String_incrementer.py
@@ -8,14 +8,16 @@ def increment_string(s: str):
     if not s:
         return "1"
     elif s.isdigit():
-        return f"{int(s) + 1}"
-    regex = r'(.+)(\d+)$'
+        digits = len(s)
+        strout = '{:0>%d}' % digits
+        return strout.format(f"{int(s) + 1}")
+    regex = r'(.+[^\d])(\d+)?$'
     matches = re.compile(regex).search(s)
-    print(f'1.. {s}:{matches.groups()}, {matches.group(0)}, {matches.group(1)}, {matches.group(2)}')
-    base = matches.group(1) or ""
+    print(f'1.. {s}:{matches}')
+    base = matches.group(1) or ''
     number = matches.group(2) or '0'
     digits = len(number)
-    print(f'2.. {s}: {base}, {matches.groups()}, {number}, {digits}')
+    print(f'2.. {s}: {base}, {matches}, {number}, {digits}')
     strout = '{}{:0>%d}' % digits
     num = int(number) + 1
     result = strout.format(base, num)
--- tests/test_String_incrementer.py
+++ tests/test_String_incrementer.py
@@ -8,9 +8,13 @@ class MyTestCase(unittest.TestCase):
     def setUpClass(cls) -> None:
         cls.t = namedtuple('test', 'entrance expected')
 
+    @classmethod
+    def tearDownClass(cls):
+        ...
+
     def test_basics0(self):
         tests = [
-            self.t("foo", "foo1"),
+            self.t("foo0", "foo1"),
             self.t("foobar1", "foobar2"),
             self.t("foobar00", "foobar01"),
             self.t("foobar001", "foobar002"),
@@ -22,21 +26,34 @@ class MyTestCase(unittest.TestCase):
         tests = [
             self.t(None, '1'),
             self.t("", "1"),
+            self.t("0", "1"),
             self.t('1', '2'),
+            self.t('009', '010')
         ]
         for i in tests:
             self.assertEqual(i.expected, increment_string(i.entrance))
 
     def test_mediums(self):
         tests = [
+            self.t("foo", "foo1"),
+            self.t("foobar9", "foobar10"),
             self.t("foobar99", "foobar100"),
+            self.t("foobar009", "foobar010"),
             self.t("foobar099", "foobar100"),
+            self.t('foo899999', 'foo900000')
         ]
         for i in tests:
             self.assertEqual(i.expected, increment_string(i.entrance))
 
     def test_advanced(self):
         tests = [
+            self.t(
+              '4Hr>ei4523LkO,+z397027080*%+2:T[502233dt%OC%=#557523Ku5n_25136474lR9~A9279M2827104N]'
+              'aWI10419070000045670589',
+              '4Hr>ei4523LkO,+z397027080*%+2:T[502233dt%OC%=#557523Ku5n_25136474lR9~A9279M2827104N]'
+              'aWI10419070000045670590'
+            ),
+            self.t('6(L1633235169', '6(L1633235170'),
             self.t("q.~2871332009", "q.~2871332010"),
             self.t('J.Z9l07816203>^jEG{|jb839460@s_ Cm"9550009', 'J.Z9l07816203>^jEG{|jb839460@s_ Cm"9550010'),
             self.t('"4On}Mb0077150000282689', '"4On}Mb0077150000282690'),
