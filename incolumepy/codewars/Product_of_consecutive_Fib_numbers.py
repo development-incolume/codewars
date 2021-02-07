57e264f64b32f1586ed3b172d0b1956a6c85bbee
Britodfbr <britodfbr@gmail.com>
2021-02-07 10:30:49 -0300

    [Product_of_consecutive_Fib_numbers] Tratativas em dom 07 fev 2021 10:30:49 -03

--- incolumepy/codewars/Product_of_consecutive_Fib_numbers.py
+++ incolumepy/codewars/Product_of_consecutive_Fib_numbers.py
@@ -8,18 +8,26 @@ import time
 @lru_cache(maxsize=64)
 def fib(n):
     # 1, 1, 2, 3, 5, ..
-    if n < 2:
-        return n
+    if n <= 2:
+        return 1
     return fib(n-1) + fib(n-2)
+
+
+@lru_cache(64)
+def fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
 
 
 def productFib(prod):
     mult = 0
     v = 0
     while mult < prod:
-        mult = fib(v) * fib(v+1)
+        mult = fibonacci(v) * fibonacci(v+1)
         if mult == prod:
-            return [fib(v), fib(v+1), True]
+            return [fibonacci(v), fibonacci(v+1), True]
         v += 1
     else:
-        return [fib(v-1), fib(v), False]
+        return [fibonacci(v-1), fibonacci(v), False]
--- tests/test_Product_of_consegutive_Fib_numbers.py
+++ tests/test_Product_of_consegutive_Fib_numbers.py
@@ -1,19 +1,40 @@
 import unittest
-from incolumepy.codewars.Product_of_consecutive_Fib_numbers import productFib, fib
+from TestBase import BaseTestCase
+from incolumepy.codewars.Product_of_consecutive_Fib_numbers import productFib, fib, fibonacci
 
 
-class MyTestCase(unittest.TestCase):
+class MyTestCase(BaseTestCase):
     def test_fib(self):
-        self.assertEqual(fib(0), 1)
-        self.assertEqual(fib(1), 1)
-        self.assertEqual(fib(2), 1)
-        self.assertEqual(fib(-1), 1)
+        tests = [
+            self.t(0, 1),
+            self.t(1, 1),
+            self.t(2, 1),
+            self.t(-1, 1),
+            self.t(5, 120),
+        ]
+        for test in tests:
+            self.assertEqual(test.expected, fib(test.entrance))
+
+    def test_fibonacci(self):
+        tests = [
+            self.t(5, 120),
+            self.t(2, 1),
+            self.t(1, 1),
+            # self.t(0, 1),
+            # self.t(-1, 1),
+        ]
+        for test in tests:
+            self.assertEqual(test.expected, fibonacci(test.entrance))
 
     def test_basics(self):
-        self.assertEqual(productFib(4895), [55, 89, True])
-        self.assertEqual(productFib(5895), [89, 144, False])
-        self.assertEqual(productFib(0), [-1, 0, False])
-        self.assertEqual(productFib(0), [0, 1, True])
+        tests = [
+            self.t(productFib(4895), [55, 89, True]),
+            self.t(productFib(5895), [89, 144, False]),
+            self.t(productFib(0), [-1, 0, False]),
+            self.t(productFib(0), [0, 1, True]),
+        ]
+        for test in tests:
+            self.assertEqual(test.expected, fib(test.entrance))
 
 
 if __name__ == '__main__':
